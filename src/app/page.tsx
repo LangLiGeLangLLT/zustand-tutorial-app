f8027f28ab38c91486879feb2e331dcfea51c2f9
llt <418797644@qq.com>
2023-08-30 23:35:09 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import BearBox from '@/components/bearBox'
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div className="container mx-auto min-h-screen p-10">
+      <h1 className="mb-5 flex justify-center">Zustand Tutorial</h1>
+      <div>
+        <BearBox />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/bearBox.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useBearStore } from '@/store/bearStore'
+
+function BearBox() {
+  const bears = useBearStore((state) => state.bears)
+  const increasePopulation = useBearStore((state) => state.increasePopulation)
+  const removeAllBears = useBearStore((state) => state.removeAllBears)
+
+  return (
+    <div className="border p-4 rounded-lg">
+      <h1>Bear Box</h1>
+      <p>bears: {bears}</p>
+      <div className="flex gap-2">
+        <button onClick={increasePopulation}>add bear</button>
+        <button onClick={removeAllBears}>remove bear</button>
+      </div>
+    </div>
+  )
+}
+
+export default BearBox
